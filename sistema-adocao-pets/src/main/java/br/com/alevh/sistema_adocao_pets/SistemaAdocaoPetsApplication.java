ca9f34e6f48d29b9dc36a2ecc6504bb0fc9467f3
alehuller <ale_huller@hotmail.com>
2025-05-05 15:04:36 -0300

    Primeiro Commit - Configuracoes Iniciais

---
+++ sistema-adocao-pets/src/main/java/br/com/alevh/sistema_adocao_pets/SistemaAdocaoPetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alevh.sistema_adocao_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdocaoPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAdocaoPetsApplication.class, args);
+	}
+
+}
