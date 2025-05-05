e551863d280876da35b911091e08f786715ba483
alehuller <ale_huller@hotmail.com>
2025-05-05 15:49:23 -0300

    Teste da Aplicacao e Mudanca do Nome da Classe de Inicializacao

--- sistema-adocao-pets/src/main/java/br/com/alevh/sistema_adocao_pets/Startup.java
+++ sistema-adocao-pets/src/main/java/br/com/alevh/sistema_adocao_pets/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaAdocaoPetsApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaAdocaoPetsApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
