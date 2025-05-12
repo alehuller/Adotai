c9594a5cf4a25b70b9f72a56441656086b43fbf6
vhugoemcruz <vhugoemcruz@gmail.com>
2025-05-12 19:16:43 -0300

    Adição de anotações do swagger na controller de usuários

--- src/main/java/br/com/alevh/sistema_adocao_pets/controller/UsuarioController.java
+++ src/main/java/br/com/alevh/sistema_adocao_pets/controller/UsuarioController.java
@@ -14,45 +14,122 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alevh.sistema_adocao_pets.model.Usuario;
 import br.com.alevh.sistema_adocao_pets.service.UsuarioService;
+import br.com.alevh.sistema_adocao_pets.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/usuarios")
+@Tag(name = "Usuários", description = "Endpoints para manipulação do registro dos usuários.")
 public class UsuarioController {
 
     private Logger logger = Logger.getLogger(UsuarioService.class.getName());
     
     private final UsuarioService usuarioService;
 
-    @GetMapping
+    @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML })
+    @Operation(summary = "Retorna todos os usuários", 
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", 
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Usuario.class))
+                                    )
+                            }),
+                            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public List<Usuario> listarUsuarios() {
         return usuarioService.findAll();
     }
 
-    @GetMapping(value = "/{id}")
-    public Usuario acharPorId(@PathVariable(value = "id") Long id) {
+    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML })
+    @Operation(summary = "Retorna o usuário de id especificado", 
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                  @Content(
+                                           mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Usuario.class))
+                                 )
+                            }),
+                            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    public Usuario acharPorId(@PathVariable Long id) {
         logger.info(String.format("Consultando usuário de id $d", id));
         return usuarioService.findById(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML },
+                 produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML })
+    @Operation(summary = "Registra um usuário", 
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                @Content(
+                                        mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Usuario.class))
+                                )
+                            }),
+                            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public Usuario registrarUsuario(@RequestBody Usuario user) {
         logger.info("Criando um usuário");
         return usuarioService.create(user);
     }
     
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML })
+    @Operation(summary = "Apaga o usuário de id especificado", 
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<?> deletarPorId(@PathVariable(value = "id") Long id){
         logger.info(String.format("Apagando usuário de id %d", id));
         usuarioService.delete(id);
         return ResponseEntity.noContent().build();
     } 
 
-    @PutMapping
+    @PutMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML },
+                produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_YML, MediaType.APPLICATION_XML })
+    @Operation(summary = "Atualiza o usuário", 
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                @Content(
+                                        mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Usuario.class))
+                                )
+                            }),
+                            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public Usuario atualizarUsuario(@RequestBody Usuario usuario) {     
         logger.info(String.format("Atualizando usuário de nome %s", usuario.getNome()));   
         return usuarioService.update(usuario);
