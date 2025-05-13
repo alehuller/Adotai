6ee771a631e33a7846de5974ba5682095cc8249a
alehuller <ale_huller@hotmail.com>
2025-05-13 20:20:34 -0300

    Adicionando VO de Usuario

---
+++ src/main/java/br/com/alevh/sistema_adocao_pets/data/vo/v1/UsuarioVO.java
@@ -0,0 +1,37 @@
+package br.com.alevh.sistema_adocao_pets.data.vo.v1;
+
+import java.io.Serializable;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@JsonPropertyOrder({ "id", "nome", "email", "cell" })
+public class UsuarioVO extends RepresentationModel<UsuarioVO> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("id")
+    @Mapping("id")
+    private Long key;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String cell;
+
+    private String cpf;
+}
