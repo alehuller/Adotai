b75d535c9fc1caadf9ef04cde105e1e09a7ada21
alehuller <ale_huller@hotmail.com>
2025-05-20 15:56:12 -0300

    Adicionando a Model de ONG

---
+++ src/main/java/br/com/alevh/sistema_adocao_pets/model/Ong.java
@@ -0,0 +1,71 @@
+package br.com.alevh.sistema_adocao_pets.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "ong")
+public class Ong implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOng;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 255)
+    private String senha;
+
+    @Column(name = "endereco", nullable = false, length = 255)
+    private String endereco;
+
+    @Column(name = "telefone", nullable = false, unique = true, length = 11)
+    private String telefone;
+
+    @Column(name = "cnpj", nullable = false, unique = true, length = 14)
+    private String cnpj;
+
+    @Column(name = "responsavel", nullable = false, length = 255)
+    private String responsavel;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ONG"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
