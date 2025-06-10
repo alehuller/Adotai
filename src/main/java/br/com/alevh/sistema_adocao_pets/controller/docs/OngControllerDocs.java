798989f3b86fa153e1a1822d334b5296d98322ed
vhugoemcruz <vhugoemcruz@gmail.com>
2025-06-10 17:59:25 -0300

    Docs do método para pesquisar a entidade por nome de usuário

--- src/main/java/br/com/alevh/sistema_adocao_pets/controller/docs/OngControllerDocs.java
+++ src/main/java/br/com/alevh/sistema_adocao_pets/controller/docs/OngControllerDocs.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.alevh.sistema_adocao_pets.data.dto.v1.AdocaoDTO;
 import br.com.alevh.sistema_adocao_pets.data.dto.v1.OngDTO;
@@ -16,76 +17,75 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface OngControllerDocs {
     @Operation(summary = "Retorna todas as ONGs", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<PagedModel<EntityModel<OngDTO>>> listarOngs(int page, int size, String direction);
 
     @Operation(summary = "Retorna a ONG de id especificado", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
-        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     OngDTO acharOngPorId(Long id);
 
     @Operation(summary = "Registra uma ONG", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     OngDTO registrarOng(OngDTO ong);
 
     @Operation(summary = "Atualiza a ONG", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     OngDTO atualizarOng(Long id, OngDTO ong);
 
     @Operation(summary = "Apaga a ONG de id especificado", responses = {
-        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<?> deletarOngPorId(Long id);
 
     @Operation(summary = "Retorna todas as adoções de uma ONG", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = AdocaoDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = AdocaoDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
-    ResponseEntity<PagedModel<EntityModel<AdocaoDTO>>> listarAdocoesPorOngId(Long id, int page, int size, String direction);
+    ResponseEntity<PagedModel<EntityModel<AdocaoDTO>>> listarAdocoesPorOngId(Long id, int page, int size,
+            String direction);
 
     @Operation(summary = "Atualização parcial da ONG", responses = {
-        @ApiResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<OngDTO> atualizarParcialOng(Long id, Map<String, Object> updates);
+
+    @Operation(summary = "Retorna a ong pelo nome de usuário pesquisado", responses = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OngDTO.class)))),
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+    })
+    OngDTO acharOngPorNomeUsuario(@PathVariable(value = "nome_usuario") String nomeUsuario);
 }
--- src/main/java/br/com/alevh/sistema_adocao_pets/controller/docs/UsuarioControllerDocs.java
+++ src/main/java/br/com/alevh/sistema_adocao_pets/controller/docs/UsuarioControllerDocs.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.alevh.sistema_adocao_pets.data.dto.v1.AdocaoDTO;
 import br.com.alevh.sistema_adocao_pets.data.dto.v1.UsuarioDTO;
@@ -15,84 +16,77 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface UsuarioControllerDocs {
-     @Operation(summary = "Retorna todos os usuários", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))
-        )),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+    @Operation(summary = "Retorna todos os usuários", responses = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<PagedModel<EntityModel<UsuarioDTO>>> listarUsuarios(int page, int size, String direction);
 
     @Operation(summary = "Retorna o usuário de id especificado", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))
-        )),
-        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     UsuarioDTO acharUsuarioPorId(Long id);
 
     @Operation(summary = "Registra um usuário", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))
-        )),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     UsuarioDTO registrarUsuario(UsuarioDTO usuario);
 
     @Operation(summary = "Apaga o usuário de id especificado", responses = {
-        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<?> deletarPorId(Long id);
 
     @Operation(summary = "Atualiza o usuário", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))
-        )),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     UsuarioDTO atualizarUsuario(Long id, UsuarioDTO usuario);
 
     @Operation(summary = "Atualização parcial do usuário", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))
-        )),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
     ResponseEntity<UsuarioDTO> atualizarParcialUsuario(Long id, Map<String, Object> updates);
 
     @Operation(summary = "Retorna todas as adoções de um usuário específico", responses = {
-        @ApiResponse(responseCode = "200", description = "Success", content = @Content(
-            mediaType = "application/json",
-            array = @ArraySchema(schema = @Schema(implementation = AdocaoDTO.class))
-        )),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = AdocaoDTO.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
     })
-    ResponseEntity<PagedModel<EntityModel<AdocaoDTO>>> listarAdocoesPorUsuarioId(Long id, int page, int size, String direction);
+    ResponseEntity<PagedModel<EntityModel<AdocaoDTO>>> listarAdocoesPorUsuarioId(Long id, int page, int size,
+            String direction);
+
+    @Operation(summary = "Retorna o usuário pelo nome de usuário pesquisado", responses = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))),
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content)
+    })
+    UsuarioDTO acharUsuarioPorNomeUsuario(@PathVariable(value = "nome_usuario") String nomeUsuario);
 }
