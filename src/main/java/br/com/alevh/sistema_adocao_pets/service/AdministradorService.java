302e0431d1b823a42a8a2cce03ed163c606486eb
alehuller <ale_huller@hotmail.com>
2025-06-30 20:49:55 -0300

    Arrumando Erro: Adicionando Anotações Necessárias na Service de Administrador

--- src/main/java/br/com/alevh/sistema_adocao_pets/service/AdministradorService.java
+++ src/main/java/br/com/alevh/sistema_adocao_pets/service/AdministradorService.java
@@ -1,5 +1,11 @@
 package br.com.alevh.sistema_adocao_pets.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AdministradorService {
 
 }
