b60aa65d8df590d21caba5c99e38ca054fc2ba10
alehuller <ale_huller@hotmail.com>
2025-08-20 22:38:55 -0300

    test: testes dos métodos de crud restantes da usuario service

--- backend/src/test/java/br/com/alevhvm/adotai/usuario/service/UsuarioServiceTest.java
+++ backend/src/test/java/br/com/alevhvm/adotai/usuario/service/UsuarioServiceTest.java
@@ -9,15 +9,20 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -35,13 +40,28 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.PagedModel.PageMetadata;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import br.com.alevhvm.adotai.administrador.dto.AdministradorDTO;
+import br.com.alevhvm.adotai.administrador.model.Administrador;
+import br.com.alevhvm.adotai.adocao.dto.AdocaoDTO;
+import br.com.alevhvm.adotai.adocao.enums.StatusAdocao;
+import br.com.alevhvm.adotai.adocao.model.Adocao;
+import br.com.alevhvm.adotai.adocao.repository.AdocaoRepository;
+import br.com.alevhvm.adotai.adocao.service.AdocaoService;
+import br.com.alevhvm.adotai.animal.dto.AnimalDTO;
 import br.com.alevhvm.adotai.animal.enums.StatusAnimal;
 import br.com.alevhvm.adotai.animal.model.Animal;
+import br.com.alevhvm.adotai.auth.dto.LoginDTO;
 import br.com.alevhvm.adotai.auth.dto.RegistroDTO;
+import br.com.alevhvm.adotai.auth.dto.TokenDTO;
 import br.com.alevhvm.adotai.auth.enums.Roles;
+import br.com.alevhvm.adotai.auth.model.LoginIdentityView;
+import br.com.alevhvm.adotai.auth.service.TokenService;
 import br.com.alevhvm.adotai.common.exceptions.ValidacaoException;
 import br.com.alevhvm.adotai.common.mapper.DozerMapper;
 import br.com.alevhvm.adotai.common.vo.CpfVO;
@@ -50,9 +70,12 @@ import br.com.alevhvm.adotai.common.vo.EnderecoVO;
 import br.com.alevhvm.adotai.common.vo.RedeVO;
 import br.com.alevhvm.adotai.ong.model.Ong;
 import br.com.alevhvm.adotai.usuario.dto.UsuarioDTO;
+import br.com.alevhvm.adotai.usuario.dto.UsuarioUpdateDTO;
 import br.com.alevhvm.adotai.usuario.model.Usuario;
 import br.com.alevhvm.adotai.usuario.repository.UsuarioRepository;
 import br.com.alevhvm.adotai.usuario.validations.UsuarioValidacao;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Validator;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
@@ -64,23 +87,38 @@ public class UsuarioServiceTest {
     private UsuarioRepository usuarioRepository;
 
     @Mock
+    private AdocaoRepository adocaoRepository;
+
+    @Mock
     private UsuarioValidacao usuarioValidacao;
 
     @Mock
     private PagedResourcesAssembler<AdministradorDTO> assembler;
 
     @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private Validator validator;
 
     private RegistroDTO registroDTO;
     private UsuarioDTO usuarioDTO;
+    private UsuarioUpdateDTO usuarioDiferente;
     private Usuario usuario;
     private Animal animalEntity;
+    private Adocao adocaoEntity;
     private Ong ong;
 
     private DescricaoVO descricaoVO;
     private RedeVO redeVO;
     private CpfVO cpf;
+    private CpfVO cpfDiferente;
     private Set<Animal> animaisFavoritos;
     private List<String> erros;
 
@@ -136,6 +174,7 @@ public class UsuarioServiceTest {
 
         cpf = new CpfVO("111222233344");
         cpf.setCpf("111222233344");
+        cpfDiferente = new CpfVO("22233344455");
 
         registroDTO = new RegistroDTO();
         registroDTO.setEmail("usuario@email.com");
@@ -169,6 +208,21 @@ public class UsuarioServiceTest {
         usuario.setAnimaisFavoritos(animaisFavoritos);
 
         erros = new ArrayList<>();
+
+        adocaoEntity = new Adocao();
+        adocaoEntity.setIdAdocao(1L);
+        adocaoEntity.setDataAdocao(LocalDate.parse("2024-07-11"));
+        adocaoEntity.setStatus(StatusAdocao.APROVADA);
+        adocaoEntity.setAnimal(animalEntity);
+        adocaoEntity.setUsuario(usuario);
+
+        usuarioDiferente = new UsuarioUpdateDTO();
+        usuarioDiferente.setNome("Usuario Nome Diferente");
+        usuarioDiferente.setEmail("usuariodiferente@email.com");
+        usuarioDiferente.setNomeUsuario("UsuarioNomeUsuarioDiferente");
+        usuarioDiferente.setFotoPerfil("Foto Usuario Diferente");
+        usuarioDiferente.setSenha("senhaCriptografada");
+        usuarioDiferente.setCell("(11) 94345-9970");
     }
 
     @Test
@@ -322,5 +376,216 @@ public class UsuarioServiceTest {
 
         assertNotNull(resultado);
         assertTrue(resultado.getContent().isEmpty());
+    }
+
+    @Test
+    void deveRetornarUsuarioPeloNomeUsuarioProcuradoComSucesso() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuario")).thenReturn(Optional.of(usuario));
+
+        UsuarioDTO resultado = usuarioService.findByNomeUsuario("UsuarioNomeUsuario");
+
+        assertNotNull(resultado);
+        assertEquals("usuario@email.com", resultado.getEmail());
+        assertTrue(resultado.getLinks().hasLink("self"));
+        assertTrue(resultado.getRequiredLink("self").getHref().endsWith("/api/v1/usuarios/nomeUsuario/" + usuario.getNomeUsuario()));
+
+        verify(usuarioRepository, times(1)).findByNomeUsuario("UsuarioNomeUsuario");
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNaoEncontrarUsuarioPorNomeUsuarioProcurado() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuarioErrado")).thenReturn(Optional.empty());
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> {
+            usuarioService.findByNomeUsuario("UsuarioNomeUsuarioErrado");
+        });
+
+        assertEquals("Usuário não encontrado.", ex.getMessage());
+    }
+
+    @Test
+    void deveRetornarUsuarioPeloIdProcuradoComSucesso() {
+        when(usuarioRepository.findById(1L)).thenReturn(Optional.of(usuario));
+
+        UsuarioDTO resultado = usuarioService.findById(1L);
+
+        assertNotNull(resultado);
+        assertEquals("usuario@email.com", resultado.getEmail());
+        assertTrue(resultado.getLinks().hasLink("self"));
+        assertTrue(resultado.getRequiredLink("self").getHref().endsWith("/api/v1/usuarios/" + usuario.getIdUsuario()));
+
+        verify(usuarioRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNaoEncontrarUsuarioPorIdProcurado() {
+        when(usuarioRepository.findById(2L)).thenReturn(Optional.empty());
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> {
+            usuarioService.findById(2L);
+        });
+
+        assertEquals("Usuário não encontrado.", ex.getMessage());
+    }
+
+    @Test
+    void deveRetornarPaginaDeAdocoesPorNomeUsuarioComSucesso() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<Adocao> adocoes = List.of(adocaoEntity);
+        Page<Adocao> adocaoPage = new PageImpl<>(adocoes, pageable, adocoes.size());
+
+        when(adocaoRepository.findAdocoesByNomeUsuario("UsuarioNomeUsuario", pageable)).thenReturn(adocaoPage);
+
+        AdocaoDTO dto = DozerMapper.parseObject(adocaoEntity, AdocaoDTO.class);
+        List<AdocaoDTO> dtoList = List.of(dto);
+        Page<AdocaoDTO> dtoPage = new PageImpl<>(dtoList, pageable, dtoList.size());
+
+        PageMetadata metadata = new PageMetadata(10, 0, 1);
+        PagedModel<EntityModel<AdocaoDTO>> pagedModelMock = PagedModel.empty(metadata);
+        when(assembler.toModel(any(Page.class), any(Link.class))).thenReturn(pagedModelMock);
+
+        PagedModel<EntityModel<AdocaoDTO>> resultado = usuarioService.findAllAdocoesByNomeUsuario("UsuarioNomeUsuario", pageable);
+
+        assertNotNull(resultado);
+        verify(adocaoRepository).findAdocoesByNomeUsuario("UsuarioNomeUsuario", pageable);
+        verify(assembler).toModel(any(Page.class), any(Link.class));
+    }
+
+    @Test
+    void deveRetornarPaginaDeAdocoesPorNomeUsuarioVazia() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Adocao> emptyPage = new PageImpl<>(Collections.emptyList(), pageable, 0);
+
+        when(adocaoRepository.findAdocoesByNomeUsuario("UsuarioNomeUsuario", pageable)).thenReturn(emptyPage);
+
+        PageMetadata metadata = new PageMetadata(10, 0, 0);
+        PagedModel<EntityModel<AdocaoDTO>> pagedModelMock = PagedModel.empty(metadata);
+        when(assembler.toModel(any(Page.class), any(Link.class))).thenReturn(pagedModelMock);
+
+        PagedModel<EntityModel<AdocaoDTO>> resultado = usuarioService.findAllAdocoesByNomeUsuario("UsuarioNomeUsuario", pageable);
+
+        assertNotNull(resultado);
+        assertTrue(resultado.getContent().isEmpty());
+    }
+
+    @Test
+    void deveLogarUsuarioComSucesso() {
+        LoginDTO loginDTO = new LoginDTO("usuario@email.com", "senha123");
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginDTO.identifier(), loginDTO.password());
+
+        Authentication auth = mock(Authentication.class);
+        LoginIdentityView principal = mock(LoginIdentityView.class);
+        when(auth.getPrincipal()).thenReturn(principal);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(auth);
+
+        when(tokenService.generateToken(principal)).thenReturn("token123");
+
+        TokenDTO resultado = usuarioService.logar(loginDTO);
+
+        assertNotNull(resultado);
+        assertEquals("token123", resultado.accessToken());
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(tokenService).generateToken(principal);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCredenciasInvalidas() {
+        LoginDTO loginDTO = new LoginDTO("usuario@email.com", "senhaErrada");
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(new BadCredentialsException("Credenciais inválidas"));
+
+        assertThrows(BadCredentialsException.class, () -> {
+            usuarioService.logar(loginDTO);
+        });
+
+        verify(authenticationManager).authenticate((any(UsernamePasswordAuthenticationToken.class)));
+        verify(tokenService, never()).generateToken(any());
+    }
+
+    @Test
+    void deveAtualizarUsuarioInteiroComSucesso() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuario")).thenReturn(Optional.of(usuario));
+        when(passwordEncoder.encode(usuarioDiferente.getSenha())).thenReturn("senhaCriptograda");
+        when(usuarioRepository.save(any(Usuario.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        UsuarioDTO resultado = usuarioService.update(usuarioDiferente, "UsuarioNomeUsuario");
+
+        assertNotNull(resultado);
+        assertEquals("UsuarioNomeUsuarioDiferente", resultado.getNomeUsuario());
+        assertTrue(resultado.getLinks().hasLink("self"));
+
+        verify(usuarioRepository).findByNomeUsuario("UsuarioNomeUsuario");
+        verify(usuarioValidacao).validateUpdate(any(Usuario.class));
+        verify(usuarioRepository).save(any(Usuario.class));
+        verify(passwordEncoder).encode(usuarioDiferente.getSenha());
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNaoEncontrarUsuarioPorNomeUsuarioNoUpdate() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuarioErrado")).thenReturn(Optional.empty());
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> {
+            usuarioService.update(usuarioDiferente, "UsuarioNomeUsuarioErrado");
+        });
+
+        assertEquals("Usuário não encontrado.", ex.getMessage());
+    }
+
+    @Test
+    void deveAtualizarUsuarioPartialComSucesso() {
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("nomeUsuario", "NovoNome");
+        updates.put("email", "novoemail@teste.com");
+
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuario")).thenReturn(Optional.of(usuario));
+
+        doNothing().when(usuarioValidacao).validatePartialUpdate("UsuarioNomeUsuario", updates);
+        when(usuarioRepository.save(any(Usuario.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(validator.validate(any(UsuarioDTO.class))).thenReturn(Collections.emptySet());
+
+        UsuarioDTO resultado = usuarioService.partialUpdate("UsuarioNomeUsuario", updates);
+
+        assertNotNull(resultado);
+        assertEquals("NovoNome", resultado.getNomeUsuario());
+        assertEquals("novoemail@teste.com", resultado.getEmail());
+        assertTrue(resultado.getLinks().hasLink("self"));
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNaoEncontrarUsuarioNoPartialUpdate() {
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("nomeUsuario", "NomeUsuarioNovo");
+
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuarioErrado")).thenReturn(Optional.empty());
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> {
+            usuarioService.partialUpdate("UsuarioNomeUsuarioErrado", updates);
+        });
+
+        assertEquals("Usuário não encontrado.", ex.getMessage());
+    }
+
+    @Test
+    void deveDeletarUsuarioComSucesso() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuario")).thenReturn(Optional.of(usuario));
+
+        usuarioService.delete("UsuarioNomeUsuario");
+
+        verify(usuarioRepository).findByNomeUsuario("UsuarioNomeUsuario");
+        verify(usuarioRepository).deleteByNomeUsuario("UsuarioNomeUsuario");
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNaoEncontrarUsuarioNoDelete() {
+        when(usuarioRepository.findByNomeUsuario("UsuarioNomeUsuarioErrado")).thenReturn(Optional.empty());
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> {
+            usuarioService.delete("UsuarioNomeUsuarioErrado");
+        });
+
+        assertEquals("Usuário não encontrado", ex.getMessage());
     }
 }
